0afb9835145e3dc91acbcd8d335fead81b517f13
codingmaven <codingmaven@gmail.com>
2016-08-07 20:17:34 +0300

    update gruntfile for heroku deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
         options: {
             jshintrc: '.jshintrc'
         },
-        src: createFolderGlobs('*.js')
+        src: createFolderGlobs(['*.js', '!*web.js'])
       }
     },
     clean: {
@@ -164,7 +164,7 @@ module.exports = function (grunt) {
         }
       }
     },
-    //Imagemin has issues on Windows.  
+    //Imagemin has issues on Windows.
     //To enable imagemin:
     // - "npm install grunt-contrib-imagemin"
     // - Comment in this section
