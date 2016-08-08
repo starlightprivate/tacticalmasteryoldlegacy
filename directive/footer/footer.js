75f730abc0563d54d72c7c9a8507d398b9d7c782
codingmaven <codingmaven@gmail.com>
2016-08-08 05:04:29 +0300

    main page added

---
+++ directive/footer/footer.js
@@ -0,0 +1,14 @@
+angular.module('tactical').directive('footer', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+
+        },
+        templateUrl: 'directive/footer/footer.html',
+        link: function(scope, element, attrs, fn) {
+
+
+        }
+    };
+});
