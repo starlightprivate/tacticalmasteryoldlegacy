93e41405e33cfca77cabfcbaea9b1072e5efa001
codingmaven <codingmaven@gmail.com>
2016-08-09 17:57:17 +0300

    update design of home page

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,12 +1,12 @@
 angular.module('tactical').controller('MainCtrl',function($scope, $uibModal){
     var vm = this;
 
-    vm.openBuyModal = function() {
+    vm.showLeadForm = function() {
         $uibModal.open({
             templateUrl: 'pages/main/buymodal/buymodal.html',
             controller: 'BuymodalCtrl'
         }).result.then(function(result){
             //do something with the result
         });
-    }
+    };
 });
