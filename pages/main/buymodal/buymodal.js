6d516edbac4a0e554aa4ad32f3f5f5ce86fb922c
Shahjada <shahjada.talukdar@gmail.com>
2016-08-10 18:48:52 +0600

    Phone Validation

--- pages/main/buymodal/buymodal.js
+++ pages/main/buymodal/buymodal.js
@@ -8,6 +8,50 @@ angular.module('tactical').controller('BuymodalCtrl', function ($scope , $uibMod
 
     $timeout(function () {
 
+      // Define new validator
+        window.FormValidation.Validator.intPhoneNumber = {
+            html5Attributes: {
+                message: 'message',
+                autoplaceholder: 'autoPlaceholder',
+                preferredcountries: 'preferredCountries',
+                utilsscript: 'utilsScript'
+            },
+
+            init: function(validator, $field, options) {
+                // Determine the preferred countries
+                var autoPlaceholder    = options.autoPlaceholder === true || options.autoPlaceholder === 'true',
+                    preferredCountries = options.preferredCountries || 'us';
+                if ('string' === typeof preferredCountries) {
+                    preferredCountries = preferredCountries.split(',');
+                }
+
+                // Attach the intlTelInput on field
+                $field.intlTelInput({
+                    utilsScript: options.utilsScript || '',
+                    autoPlaceholder: autoPlaceholder,
+                    preferredCountries: preferredCountries
+                });
+
+                // Revalidate the field when changing the country
+                var $form     = validator.getForm(),
+                    fieldName = $field.attr('data-fv-field');
+                $form.on('click.country.intphonenumber', '.country-list', function() {
+                    $form.formValidation('revalidateField', fieldName);
+                });
+            },
+
+            destroy: function(validator, $field, options) {
+                $field.intlTelInput('destroy');
+
+                validator.getForm().off('click.country.intphonenumber');
+            },
+
+            validate: function(validator, $field, options) {
+                return $field.val() === '' || $field.intlTelInput('isValidNumber');
+            }
+        };
+        
+
         $('#leadForm').formValidation({
             framework: 'bootstrap',
             icon: {
@@ -58,6 +102,20 @@ angular.module('tactical').controller('BuymodalCtrl', function ($scope , $uibMod
                     regexp: {
                         regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
                         message: 'This is not a valid email address'
+                    }
+                }
+            },
+
+            phoneNumber: {
+                validators: {
+                    notEmpty: {
+                        message: 'The phone number is required'
+                    },
+                    intPhoneNumber: {
+                        utilsScript: '/libs/utils.js',
+                        autoPlaceholder: true,
+                        preferredCountries: 'fr,us,gb',
+                        message: 'The phone number is not valid'
                     }
                 }
             }
