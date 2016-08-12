fbf8d5e26f8011401723210098a975975502bcb3
Shahjada <shahjada.talukdar@gmail.com>
2016-08-13 03:38:54 +0600

    Use Div and Remove Form

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -1,4 +1,8 @@
-angular.module('tactical').controller('CheckoutCtrl',function($scope){
+angular.module('tactical').controller('CheckoutCtrl',['$scope','$rootScope', '$state', '$stateParams', function ($scope,$rootScope, $state, $stateParams) {
+	console.log('CheckoutCtrl is running...');
 
-
-});
+	
+	
+  console.log($rootScope.modalData);
+	
+}]);
