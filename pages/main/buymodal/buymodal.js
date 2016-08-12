7bb196cd16f7165d81d8537c87e9a2a8027c303e
Shahjada <shahjada.talukdar@gmail.com>
2016-08-13 04:06:16 +0600

    Use Custom validation

--- pages/main/buymodal/buymodal.js
+++ pages/main/buymodal/buymodal.js
@@ -120,7 +120,10 @@ angular.module('tactical').controller('BuymodalCtrl', ['$scope','$rootScope', '$
         if (checkvalidation2()) {
             // Do Proper Logic Stuffs
             $rootScope.modalData = $scope.formData;
-            $state.go('checkout', { modalData: $scope.formData });
+            $('#myModal').modal('hide');
+            setTimeout(function() {
+                $state.go('checkout', { modalData: $scope.formData });
+            }, 411);
             //return true;
             
         } else {
@@ -129,143 +132,147 @@ angular.module('tactical').controller('BuymodalCtrl', ['$scope','$rootScope', '$
         }
         
     };
-    
+
     $timeout(function () {
+        $('input[name=phoneNumber]').mask('000-000-0000', { 'translation': { 0: { pattern: /[0-9*-]/ } } });
+    }, 0);
 
-        var isFormDirty = false;
+    // $timeout(function () {
 
-        // Define new validator
-        window.FormValidation.Validator.intPhoneNumber = {
-            html5Attributes: {
-                message: 'message',
-                autoplaceholder: 'autoPlaceholder',
-                preferredcountries: 'preferredCountries',
-                utilsscript: 'utilsScript'
-            },
+    //     var isFormDirty = false;
 
-            init: function(validator, $field, options) {
-                // Determine the preferred countries
-                var autoPlaceholder    = options.autoPlaceholder === true || options.autoPlaceholder === 'true',
-                    preferredCountries = options.preferredCountries || 'us';
-                if ('string' === typeof preferredCountries) {
-                    preferredCountries = preferredCountries.split(',');
-                }
+    //     // Define new validator
+    //     window.FormValidation.Validator.intPhoneNumber = {
+    //         html5Attributes: {
+    //             message: 'message',
+    //             autoplaceholder: 'autoPlaceholder',
+    //             preferredcountries: 'preferredCountries',
+    //             utilsscript: 'utilsScript'
+    //         },
 
-                // Attach the intlTelInput on field
-                $field.intlTelInput({
-                    utilsScript: options.utilsScript || '',
-                    autoPlaceholder: autoPlaceholder,
-                    preferredCountries: preferredCountries
-                });
+    //         init: function(validator, $field, options) {
+    //             // Determine the preferred countries
+    //             var autoPlaceholder    = options.autoPlaceholder === true || options.autoPlaceholder === 'true',
+    //                 preferredCountries = options.preferredCountries || 'us';
+    //             if ('string' === typeof preferredCountries) {
+    //                 preferredCountries = preferredCountries.split(',');
+    //             }
 
-                // Revalidate the field when changing the country
-                var $form     = validator.getForm(),
-                    fieldName = $field.attr('data-fv-field');
-                $form.on('click.country.intphonenumber', '.country-list', function() {
-                    $form.formValidation('revalidateField', fieldName);
-                });
-            },
+    //             // Attach the intlTelInput on field
+    //             $field.intlTelInput({
+    //                 utilsScript: options.utilsScript || '',
+    //                 autoPlaceholder: autoPlaceholder,
+    //                 preferredCountries: preferredCountries
+    //             });
 
-            destroy: function(validator, $field, options) {
-                $field.intlTelInput('destroy');
+    //             // Revalidate the field when changing the country
+    //             var $form     = validator.getForm(),
+    //                 fieldName = $field.attr('data-fv-field');
+    //             $form.on('click.country.intphonenumber', '.country-list', function() {
+    //                 $form.formValidation('revalidateField', fieldName);
+    //             });
+    //         },
 
-                validator.getForm().off('click.country.intphonenumber');
-            },
+    //         destroy: function(validator, $field, options) {
+    //             $field.intlTelInput('destroy');
 
-            validate: function(validator, $field, options) {
-                return $field.val() === '' || $field.intlTelInput('isValidNumber');
-            }
-        };
+    //             validator.getForm().off('click.country.intphonenumber');
+    //         },
+
+    //         validate: function(validator, $field, options) {
+    //             return $field.val() === '' || $field.intlTelInput('isValidNumber');
+    //         }
+    //     };
 
 
-        $('#leadForm').formValidation({
-            framework: 'bootstrap4',
-            icon: {
-            valid: 'fa fa-check',
-            invalid: 'fa fa-remove',
-            validating: 'fa fa-refresh'
-            },
-            fields: {
-            fullname: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Full name is required'
-                    },
-                    stringLength: {
-                        min: 1,
-                        max: 30,
-                        message: 'The name must be more than 6 and less than 30 characters long. '
-                    },
-                    regexp: {
-                        regexp: /^[a-zA-Z \.]+$/,
-                        message: 'The name can only consist of alphabetical'
-                    }
-                }
-            },
+    //     $('#leadForm').formValidation({
+    //         framework: 'bootstrap4',
+    //         icon: {
+    //         valid: 'fa fa-check',
+    //         invalid: 'fa fa-remove',
+    //         validating: 'fa fa-refresh'
+    //         },
+    //         fields: {
+    //         fullname: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: 'The Full name is required'
+    //                 },
+    //                 stringLength: {
+    //                     min: 1,
+    //                     max: 30,
+    //                     message: 'The name must be more than 6 and less than 30 characters long. '
+    //                 },
+    //                 regexp: {
+    //                     regexp: /^[a-zA-Z \.]+$/,
+    //                     message: 'The name can only consist of alphabetical'
+    //                 }
+    //             }
+    //         },
 
-            email: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Email is required'
-                    },
-                    regexp: {
-                        regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
-                        message: 'This is not a valid email address'
-                    }
-                }
-            },
+    //         email: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: 'The Email is required'
+    //                 },
+    //                 regexp: {
+    //                     regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
+    //                     message: 'This is not a valid email address'
+    //                 }
+    //             }
+    //         },
 
-            // phoneNumber: {
-            //     validators: {
-            //         notEmpty: {
-            //             message: 'The phone number is required'
-            //         },
-            //         intPhoneNumber: {
-            //             utilsScript: '/libs/utils.js',
-            //             autoPlaceholder: true,
-            //             preferredCountries: 'fr,us,gb',
-            //             message: 'The phone number is not valid'
-            //         }
-            //     }
-            // }
-            // phone number field
-            phoneNumber: {
-                validMessage: 'The phone number looks great',
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply a phone number so we can call if there are any problems using this address.'
-                    },
-                    stringLength: {
-                        min: 10,
-                        message: 'Not a valid 10-digit US phone number (must not include spaces or special characters)'
-                    }
-                }
-            },
-          }
-        })
-        // .on('success.field.fv', function(e, data) {
-        //     var length = data.fv.getInvalidFields().length;
-        //     console.log('Firedddddddddddddddd', length);
-        //     if (!isFormDirty) {
-        //         data.fv.disableSubmitButtons(true);
-        //         isFormDirty = true;
-        //         return;
-        //     }
-        //     if (length > 0) {    // There is invalid field
-        //         data.fv.disableSubmitButtons(true);
-        //     }
-        // })
-        .on('click', '.country-list', function() {
-            $('#leadForm').formValidation('revalidateField', 'phoneNumber');
-        });
+    //         // phoneNumber: {
+    //         //     validators: {
+    //         //         notEmpty: {
+    //         //             message: 'The phone number is required'
+    //         //         },
+    //         //         intPhoneNumber: {
+    //         //             utilsScript: '/libs/utils.js',
+    //         //             autoPlaceholder: true,
+    //         //             preferredCountries: 'fr,us,gb',
+    //         //             message: 'The phone number is not valid'
+    //         //         }
+    //         //     }
+    //         // }
+    //         // phone number field
+    //         phoneNumber: {
+    //             validMessage: 'The phone number looks great',
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: 'Please supply a phone number so we can call if there are any problems using this address.'
+    //                 },
+    //                 stringLength: {
+    //                     min: 10,
+    //                     message: 'Not a valid 10-digit US phone number (must not include spaces or special characters)'
+    //                 }
+    //             }
+    //         },
+    //       }
+    //     })
+    //     // .on('success.field.fv', function(e, data) {
+    //     //     var length = data.fv.getInvalidFields().length;
+    //     //     console.log('Firedddddddddddddddd', length);
+    //     //     if (!isFormDirty) {
+    //     //         data.fv.disableSubmitButtons(true);
+    //     //         isFormDirty = true;
+    //     //         return;
+    //     //     }
+    //     //     if (length > 0) {    // There is invalid field
+    //     //         data.fv.disableSubmitButtons(true);
+    //     //     }
+    //     // })
+    //     .on('click', '.country-list', function() {
+    //         $('#leadForm').formValidation('revalidateField', 'phoneNumber');
+    //     });
 
 
         
-        $('input[name=phoneNumber]').mask('000-000-0000', {'translation': {0: {pattern: /[0-9*-]/}}});
+    //     $('input[name=phoneNumber]').mask('000-000-0000', {'translation': {0: {pattern: /[0-9*-]/}}});
        
 
 
-    }, 0);
+    // }, 0);
 
 
 }]);
