0994e8c5d640d79d0fe5ea7e2992760f8841e1d2
Shahjada <shahjada.talukdar@gmail.com>
2016-08-15 19:05:38 +0600

    name validMessage

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -41,6 +41,7 @@ angular.module('tactical').controller('CheckoutCtrl', ['$scope','$state','$state
                                 message: 'The name is required'
                             },
                             stringLength: {
+                                min : 6,
                                 max: 30,
                                 message: 'The name must be more than 6 and less than 30 characters long. '
                             },
