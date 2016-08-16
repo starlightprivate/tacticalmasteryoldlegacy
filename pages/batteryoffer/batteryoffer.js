e1388f5ccc232cc3aa9d0bfc9db61930061eb088
Shahjada <shahjada.talukdar@gmail.com>
2016-08-17 01:38:04 +0600

    Battery  Button Fired

--- pages/batteryoffer/batteryoffer.js
+++ pages/batteryoffer/batteryoffer.js
@@ -2,4 +2,12 @@ angular.module('tactical').controller('BatteryOfferCtrl', ['$scope','$state','$s
  
     console.log("BatteryOfferCtrl Running", $stateParams.modalData);
     
+    $scope.doUpsellYes = function (productId, itemPrice) {
+        var usParams = {};
+        usParams['orderId'] = window.myOrderID;
+        usParams['productQty'] = 1;
+        var nextPage = '/tacticalsales/receipt.html?orderId=' + window.myOrderID;
+
+    };
+    
 }]);
