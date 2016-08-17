dd8f67ce75c76e9c9e55686e8971cd2a4fdca656
Shahjada <shahjada.talukdar@gmail.com>
2016-08-17 12:52:00 +0600

    Build Error

--- pages/batteryoffer/batteryoffer.js
+++ pages/batteryoffer/batteryoffer.js
@@ -2,7 +2,7 @@ angular.module('tactical').controller('BatteryOfferCtrl', ['$scope','$state','$s
  
     console.log("BatteryOfferCtrl Running", $stateParams.orderData);
 
-    var orderData = {"dateCreated":"2016-08-16 15:54:37","f_address1":"aas wewe wrewr 21","f_city":"NY","f_name":"Shahjada","f_emailAddress":"par@test.com","f_phoneNumber":"3343343333","orderId":"CD814E6B80","f_postalCode":"12312","initialProductId":"6"}
+    //var orderData = {"dateCreated":"2016-08-16 15:54:37","f_address1":"aas wewe wrewr 21","f_city":"NY","f_name":"Shahjada","f_emailAddress":"par@test.com","f_phoneNumber":"3343343333","orderId":"CD814E6B80","f_postalCode":"12312","initialProductId":"6"}
     
     $scope.doUpsellYes = function (productId, itemPrice) {
         var usParams = {};
@@ -10,9 +10,9 @@ angular.module('tactical').controller('BatteryOfferCtrl', ['$scope','$state','$s
         usParams['productQty'] = 1;
         //var nextPage = '/tacticalsales/receipt.html?orderId=' + window.myOrderID;
 
-        ga('send', 'event', 'Purchase', 'Upsell (Battery)', 'conversion value', itemPrice || 0);
+        //ga('send', 'event', 'Purchase', 'Upsell (Battery)', 'conversion value', itemPrice || 0);
         productId = productId || '12';
-        nextPage = '/tacticalsales/us_headlampoffer.html?orderId=' + window.myOrderID;
+        var nextPage = '/tacticalsales/us_headlampoffer.html?orderId=' + window.myOrderID;
 
     };
 
